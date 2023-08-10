f70828e6b4cc678b961e0ca2e074b9e8de49c7ff
Tanja <git@tanja.pw>
2023-08-10 22:02:31 +0200

    `/sounds` may work

--- sounds/src/main.rs
+++ sounds/src/main.rs
@@ -1,14 +1,12 @@
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Request, Response};
 use lazy_static::lazy_static;
 use serde::Serialize;
-use serde_json::json;
+use serde_json::{json, Value};
 use std::fs;
-use std::net::{SocketAddr, TcpListener};
-use std::path::{Path, PathBuf};
+
+use std::path::Path;
 use std::process::Command;
 use std::sync::Mutex;
-use std::thread;
 
 lazy_static! {
     static ref AUDIO_LOCK: Mutex<()> = Mutex::new(());
@@ -22,7 +20,11 @@ struct Sound {
     path: String,
 }
 
-fn play_sound_from_path(filepath: &str) {
+/// Plays a sound from a given path using `mplayer`
+///
+/// Spawns a new child process and returns immediately.  
+/// Multiple sounds are prevented by using a global lock.
+pub fn play_sound_from_path(filepath: &str) {
     let _lock = AUDIO_LOCK.lock().unwrap();
     Command::new("mplayer")
         .args(&[
@@ -54,12 +56,10 @@ fn get_sounds_list() -> Vec<Sound> {
     sounds
 }
 
-async fn sounds_handler(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+async fn sounds_handler() -> Json<Value> {
     let sounds = get_sounds_list();
-    let response = json!({ "sounds": sounds });
-    Ok(Response::new(Body::from(
-        serde_json::to_string(&response).unwrap(),
-    )))
+    let response = json!(sounds);
+    Json(response)
 }
 
 /*
@@ -118,15 +118,17 @@ async fn main() {
 }
 */
 
-use axum::{routing::get, Router};
+use axum::{routing::get, Json, Router};
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/sounds", get(sounds_handler))
+        .route("/", get(|| async { "Hello, World!" }));
 
     // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:420420".parse().unwrap())
+    axum::Server::bind(&"0.0.0.0:4242".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
