0fb1ef0aaf24f129c7aa7578e93faf98a55a3252
Tanja <git@tanja.pw>
2023-09-05 03:03:13 +0200

    Implement basic demo

--- projector/src/main.rs
+++ projector/src/main.rs
@@ -1,13 +1,22 @@
-// _Creston_ is probably a trademarked name of some legal entity, with which we are not affiliated with whatsoever.
-pub mod commands;
+use std::net::Ipv4Addr;
+
+use protocol::commands;
+use tokio::{io::AsyncWriteExt, net::TcpStream};
+
+pub mod protocol;
 
 // This is hard-coded for now
 // TODO make this configurable to account for DHCP
-const IP_ADDRESS: &str = "192.168.127.26";
+const IP_ADDRESS: Ipv4Addr = Ipv4Addr::new(192, 168, 127, 26);
 
 // TODO make this configurable
 const PORT: u16 = 41794;
 
-fn main() {
-    println!("WIP (see repository)");
+#[tokio::main]
+async fn main() {
+    // Connect to the projector
+    let mut stream = TcpStream::connect((IP_ADDRESS, PORT)).await.unwrap();
+
+    // Turn the projector on
+    stream.write_all(&commands::power::ON).await.unwrap();
 }
